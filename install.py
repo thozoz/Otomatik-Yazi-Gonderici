d6f4f0aad02eca0ab72d72a8234bc33afb69f048
thozoz <efe052006@gmail.com>
2025-06-11 13:07:44 +0300

    pysimplegui güncellemesi

---
+++ install.py
@@ -0,0 +1,5 @@
+import os
+
+# PySimpleGUI'yi kendi sitesinden yükleme komutu
+os.system("python -m pip install --force-reinstall --extra-index-url https://PySimpleGUI.net/install PySimpleGUI")
+# "python -m pip install --force-reinstall --extra-index-url https://PySimpleGUI.net/install PySimpleGUI" komutunu venv de de çalıştırabilirsiniz.
